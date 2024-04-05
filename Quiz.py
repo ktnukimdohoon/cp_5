f93511ff967b37acfcd1a3ffacf325b217e0be09
ktnukimdohoon <163504699+ktnukimdohoon@users.noreply.github.com>
2024-04-05 15:29:31 +0900

    5주차
    5주차 퀴즈

--- Quiz.py
+++ Quiz.py
@@ -1,1 +1,13 @@
-werwerwerwer
+score=int(input("성적을 입력하세요: "))
+
+if score>=71:
+    print("A")
+
+elif score>=41:
+    print("B")
+
+elif score>=11:
+    print("c")
+
+elif score<=10:
+    print("D")
